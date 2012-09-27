4884776a91762b940e8b532283e667896fcbec75
Kirstine <Kirstine@10.99.17.212>
2012-09-27 10:24:31 +0200

    Der er næsten sket noget

---
+++ src/flow2/Flow2Controller.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package flow2;
+import java.io.IOException;
+/**
+ *
+ * @author claraparidonrasmussen
+ */
+public class Flow2Controller
+{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException
+    {
+        // TODO code application logic here
+    }
+}
