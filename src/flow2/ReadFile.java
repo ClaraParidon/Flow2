75df188c804cf902508eb051bc74188bd8a469c0
Kirstine <Kirstine@10.99.17.212>
2012-09-27 10:33:21 +0200

    Der er næsten sket noget

--- src/flow2/ReadFile.java
+++ src/flow2/ReadFile.java
@@ -33,4 +33,20 @@ public class ReadFile
         textReader.close();
         return textData;
     }
+    int readLines() throws IOException
+    {
+        FileReader file_to_read = new FileReader(path);
+        BufferedReader bf = new BufferedReader(file_to_read);
+        
+        String aLine;
+        int numberOfLines = 0;
+        
+        while((aLine = bf.readLine()) !=null)
+        {
+            numberOfLines++;
+        }
+        
+        bf.close();
+        return numberOfLines;
+    }
 }
